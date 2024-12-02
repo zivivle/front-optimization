5a97eeedac8cdab93d40c1b7d7adafeb99da7964
zivivle <123868471+zivivle@users.noreply.github.com>
2024-12-02 22:02:32 +0900

    feat: 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import logo from './logo.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
 
@@ -9,20 +6,7 @@ const cx = classNames.bind(styles)
 function App() {
   return (
     <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   )
 }
